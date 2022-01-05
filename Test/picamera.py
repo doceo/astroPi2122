112100d7736814f3b0ab1b4732116d3ffba8c70a
Bocchetty <bocchettifrancesco06@outlook.it>
2022-01-05 10:51:41 +0100

    Aggiunta funzione picamera

---
+++ Test/picamera.py
@@ -0,0 +1,13 @@
+from picamera import *
+import time
+
+camera = PiCamera()
+camera.start_preview()
+
+while(1):
+    for i in range(9):
+    time.sleep(1800)
+#	data = '{:>6.0f}'.format(time.monotonic())
+	camera.capture('image/image%s.jpg' % i)
+  
+camera.stop_preview()
