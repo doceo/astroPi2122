e20b3fe75f5b7d4d893a5b65aa9b72e13bf2c683
Bocchetty <bocchettifrancesco06@outlook.it>
2022-01-05 11:11:34 +0100

    Update picamera.py

--- Test/picamera.py
+++ Test/picamera.py
@@ -1,13 +1,21 @@
 from picamera import *
 import time
 
+#creazione della variabile camera
 camera = PiCamera()
+
+#inizio registrazione
 camera.start_preview()
 
+#ciclo di while che stabilisce l'intervallo in secondi tra gli scatti
 while(1):
     for i in range(9):
-    time.sleep(1800)
+        time.sleep(1800)
+
 #	data = '{:>6.0f}'.format(time.monotonic())
-	camera.capture('image/image%s.jpg' % i)
-  
+
+#cattura immagine
+camera.capture('image/image%s.jpg' % i)
+
+#termine registrazione
 camera.stop_preview()
