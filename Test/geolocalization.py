534b8069044279237e21b9c98f7afb23b88c9a67
Raffaele135 <72888664+Raffaele135@users.noreply.github.com>
2022-01-13 13:11:00 +0000

    Update geolocalization.py

--- Test/geolocalization.py
+++ Test/geolocalization.py
@@ -1,14 +1,29 @@
 from orbit import ISS
+from sense_hat import SenseHat
 
-def geographical_values():
-    # Compute the coordinates of the Earth location directly beneath the ISS
+# Set up Sense Hat
+sense = SenseHat()
+
+# Compute the coordinates of the Earth location directly beneath the ISS
     location = ISS.coordinates() 
 
-    # Valori espressi in DMS
-    print(f'Lat: {location.latitude.signed_dms()}, Long: {location.longitude.signed_dms()}') 
+# Latitudine e longtudine espressi in DMS
+Latitudine = location.latitude.signed_dms
+Longitudine = location.longitude.signed_dms
     
-    # Valori espressi in DD
-    print(f'Lat: {location.latitude.degrees:.1f}, Long: {location.longitude.degrees:.1f}')
+# Latitudine e longtudine espressi in DD
+Latitudine = location.latitude.degrees:.1f
+Longitudine = location.longitude.degrees:.1f
 
-    # Unico modo per esprimere elevazione
-    print(f'Elevation: {location.elevation.km}')
+# Elevazione
+Elevation = location.elevation.km
+    
+# Umidità 
+umidity = sense.umidity
+    
+# Temperature
+temperature = sense.temperature
+    
+    
+    
+    
