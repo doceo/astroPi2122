6515a3a586239c38779f4b546eaffdc78019cb7c
Bocchetty <bocchettifrancesco06@outlook.it>
2022-01-13 22:24:22 +0100

    Update capture.py

--- Test/capture.py
+++ Test/capture.py
@@ -6,7 +6,6 @@ def capture():
     base_folder = Path(__file__).parent.resolve()
     camera = PiCamera()
     camera.resolution = (1296,972)
-    camera.start_preview()
     # Camera warm-up time
     sleep(2)
     camera.capture(f"{base_folder}/datatime.now.jpg")
