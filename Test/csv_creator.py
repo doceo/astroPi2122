a8b9d4c1adc28988fae18a680526e23293011898
Raffaele135 <72888664+Raffaele135@users.noreply.github.com>
2022-01-13 13:27:45 +0000

    Create csv_creator.py

---
+++ Test/csv_creator.py
@@ -0,0 +1,25 @@
+import csv
+from phatlib import Path
+
+base_folder = Path(__file__).parent.resolve()
+
+# Initialise the CSV file
+data_file = base_folder/'data.csv'
+create_csv_file(data_file)
+
+# Open the csv and modify it
+with open(data_file, 'w', buffering=1) as f:
+    writer = csv.writer(f)
+    header = ("Date/time", "Latitude", "Longitude", "Temperature", "Humidity")
+    writer.writerow(header)
+  
+''' ciclo dove quando scatti una foto scrivere una riga del csv
+# Save the data to the file
+data = (
+  datatime.now(),
+  latitude,
+  longitude,
+  temperature,
+  humidity,
+)
+add_csv_data(data_file, data)
