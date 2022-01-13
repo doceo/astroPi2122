f8fddd11e42ddcbc8513a519c86909ad4e1f8c89
Bocchetty <bocchettifrancesco06@outlook.it>
2022-01-13 22:20:44 +0100

    Aggiunta funzione capture

---
+++ Test/capture.py
@@ -0,0 +1,12 @@
+from time import sleep
+from picamera import PiCamera
+from pathlib import Path
+
+def capture():
+    base_folder = Path(__file__).parent.resolve()
+    camera = PiCamera()
+    camera.resolution = (1296,972)
+    camera.start_preview()
+    # Camera warm-up time
+    sleep(2)
+    camera.capture(f"{base_folder}/datatime.now.jpg")
