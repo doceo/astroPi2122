58d886a190c18f2eb339765bb5f74bc59443cad4
Raffaele135 <mariano.raffaele@liceoviconapoli.it>
2022-01-16 16:56:26 +0100

    Update csv_creator.py

--- Test/csv_creator.py
+++ Test/csv_creator.py
@@ -1,19 +1,15 @@
 import csv
-from phatlib import Path
+'import geolocalization'
 
-base_folder = Path(__file__).parent.resolve()
 
-# Initialise the CSV file
-data_file = base_folder/'data.csv'
-create_csv_file(data_file)
 
 # Open the csv and modify it
-with open(data_file, 'w', buffering=1) as f:
+with open('data_file', 'w', buffering=1) as f:
     writer = csv.writer(f)
     header = ("Date/time", "Latitude", "Longitude", "Temperature", "Humidity")
     writer.writerow(header)
-  
-''' ciclo dove quando scatti una foto scrivere una riga del csv
+
+'''
 # Save the data to the file
 data = (
   datatime.now(),
@@ -22,4 +18,5 @@ data = (
   temperature,
   humidity,
 )
-add_csv_data(data_file, data)'''
+add_csv_data(data_file, data)
+'''
