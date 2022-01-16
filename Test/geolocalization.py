4edcc76a817320803cebc4d24079ae94e2b76158
Raffaele135 <mariano.raffaele@liceoviconapoli.it>
2022-01-16 16:56:30 +0100

    Update geolocalization.py

--- Test/geolocalization.py
+++ Test/geolocalization.py
@@ -1,22 +1,18 @@
 from orbit import ISS
-from sense_hat import SenseHat
+from pisense import SenseHat
 
 # Set up Sense Hat
 sense = SenseHat()
 
 # Compute the coordinates of the Earth location directly beneath the ISS
-    location = ISS.coordinates() 
+location = ISS.coordinates() 
 
 # Latitudine e longtudine espressi in DMS
-Latitudine = location.latitude.signed_dms
-Longitudine = location.longitude.signed_dms
+latitude = location.latitude.signed_dms
+longitude = location.longitude.signed_dms
     
-# Latitudine e longtudine espressi in DD
-Latitudine = location.latitude.degrees:.1f
-Longitudine = location.longitude.degrees:.1f
-
 # Elevazione
-Elevation = location.elevation.km
+elevation = location.elevation.km
     
 # Umidità 
 umidity = sense.umidity
