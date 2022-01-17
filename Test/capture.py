4c020e91eaa2cd118a701c9816f33b6e087d5db9
astropi <astropi@liceoviconapoli.it>
2022-01-17 18:50:56 +0100

    capture

--- Test/capture.py
+++ Test/capture.py
@@ -1,11 +1,19 @@
 from time import sleep
 from picamera import PiCamera
 from pathlib import Path
+from time import time
 
 def capture():
+    
     base_folder = Path(__file__).parent.resolve()
+    name_image = str(base_folder) + "/images/" + str(time()) +".jpg"
     camera = PiCamera()
     camera.resolution = (1296,972)
     # Camera warm-up time
     sleep(2)
-    camera.capture(f"{base_folder}/datatime.now.jpg")
+    camera.capture(f"{name_image}")
+
+if __name__ == "__main__":
+
+    capture()
+
