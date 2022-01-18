339040dfee18d7f5d8d5d94bfcde534721746de5
Raffaele135 <72888664+Raffaele135@users.noreply.github.com>
2022-01-18 12:17:44 +0100

    Update csv_creator.py

--- Test/csv_creator.py
+++ Test/csv_creator.py
@@ -1,7 +1,13 @@
 import csv
-'import geolocalization'
+from orbit import ISS
+from pisense import SenseHat
 
+# Set up Sense Hat
+sense = SenseHat()
 
+# Compute the coordinates of the Earth location directly beneath the ISS
+location = ISS.coordinates() 
+   
 
 # Open the csv and modify it
 with open('data_file', 'w', buffering=1) as f:
@@ -9,14 +15,12 @@ with open('data_file', 'w', buffering=1) as f:
     header = ("Date/time", "Latitude", "Longitude", "Temperature", "Humidity")
     writer.writerow(header)
 
-'''
 # Save the data to the file
 data = (
-  datatime.now(),
-  latitude,
-  longitude,
-  temperature,
-  humidity,
+  location.latitude.signed_dma,
+  location.longitude.signed_dms,
+  location.elevation.km
+  sense.temperature,
+  sense.humidity,
 )
 add_csv_data(data_file, data)
-'''
