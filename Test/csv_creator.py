dbbf0316f8f51d17de5da763546bc22cf29150d3
Diomede Mazzone <diomede.mazzone@gmail.com>
2022-02-02 22:59:01 +0100

    csv

--- Test/csv_creator.py
+++ Test/csv_creator.py
@@ -5,19 +5,20 @@ from pathlib import Path
 from time import sleep
 from datetime import datetime, timedelta
 
-# Set up Sense Hat
-#sense = SenseHat()
+# Initialise the CSV file
+base_folder = Path(__file__).parent.resolve()
+data_file = base_folder/'data.csv'
+
 
 # Compute the coordinates of the Earth location directly beneath the ISS
 location = ISS.coordinates() 
 print(location)
 
-
 # Define the function that create the CSV and write the firts row
 def create_csv(data_file):
     with open(data_file, 'w') as f:
         writer = csv.writer(f)
-        header = ("Date/time", "Latitude", "Longitude", "Elevation", "Temperature", "Humidity")
+        header = ("Date/time", "Latitude", "Longitude", "Elevation")
         writer.writerow(header)
 
 # Define the function to write the other row with the values
@@ -27,27 +28,15 @@ def add_csv_data(data_file, data):
         writer.writerow(data)
 
 
-# Initialise the CSV file
-base_folder = Path(__file__).parent.resolve()
-data_file = base_folder/'data.csv'
-create_csv(data_file)
 
+if __name__ == '__main__':
 
+    create_csv(data_file)
 
-#Questo ciclo può essere un calco tramite il quale si può creare la funzione main
+    # Record the start and current time
+    start_time = datetime.now()
+    now_time = datetime.now()
 
-
-# Record the start and current time
-start_time = datetime.now()
-now_time = datetime.now()
-
-# Run a loop for three hours
-while (now_time < start_time + timedelta(minutes=180)):
-    if light == 'Light':
-        # funzione capture
-        row = (datetime.now(), location.latitude.signed_dma, location.longitude.signed_dms, location.elevation.km, sense.temperature, sense.humidity)
-        'row = (datetime.now(), location.latitude.degrees, location.longitude.degrees, location.elevation.km, sense.temperature, sense.humidity)'
-        add_csv_data(data_file, row)
-        sleep(30)
-        # Update the current time
-        now_time = datetime.now()
+    # funzione capture
+    row = (datetime.now(), location.latitude.signed_dma, location.longitude.signed_dms, location.elevation.km)
+    add_csv_data(data_file, row)
