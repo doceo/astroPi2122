3ca4ec6c19646420adef0b3141635426edf15f77
Diomede Mazzone <diomede.mazzone@gmail.com>
2022-02-04 11:57:11 +0100

    aggiunte le prime funzioni a functions.py

--- functions.py
+++ functions.py
@@ -2,14 +2,17 @@ from random import randint
 from orbit import ISS
 from skyfield.api import load
 
-
-def funcTest():
-    return "facciamo un test con numero random: " + str(randint(1, 100))
+import csv
+from pathlib import Path
+from time import sleep
+from datetime import datetime, timedelta
 
 
 ephemeris = load('de421.bsp')
 timescale = load.timescale()
 
+# Compute the coordinates of the Earth location directly beneath the ISS
+location = ISS.coordinates() 
 
 def dayNight():
     t = timescale.now()
@@ -17,6 +20,21 @@ def dayNight():
         return True
     else:
         return False
+
+# Define the function that create the CSV and write the firts row
+def create_csv(data_file):
+    with open(data_file, 'w') as f:
+        writer = csv.writer(f)
+        header = ("Date/time", "Latitude", "Longitude", "Elevation")
+        writer.writerow(header)
+
+# Define the function to write the other row with the values
+def add_csv_data(data_file, data):
+    with open(data_file, 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow(data)
+
+
 
 
 if __name__ == '__main__':
