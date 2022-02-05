38c916775579814983b9e6cf88e44b09a0cf89f7
renatogallo27 <gallo.renato.s27@liceoviconapoli.it>
2022-02-05 19:31:05 +0100

    Aggiustamenti
    Adesso le funzioni di ndvi.py sono state rimosse dal functions.py e ndvi.py è stato spostano nella root.

--- functions.py
+++ functions.py
@@ -9,11 +9,6 @@ from pathlib import Path
 from time import sleep
 from datetime import datetime, timedelta
 
-# moduli utili al ndvi
-import cv2
-import numpy as np
-from fastiecm import fastiecm
-
 
 def capture(imName):
     name_image = imName + ".jpg"
@@ -45,62 +40,6 @@ def add_csv_data(data_file, data):
     with open(data_file, 'a') as f:
         writer = csv.writer(f)
         writer.writerow(data)
-
-
-def display(image, image_name):
-    image = np.array(image, dtype=float) / float(255)
-    shape = image.shape
-    height = int(shape[0] / 2)
-    width = int(shape[1] / 2)
-    image = cv2.resize(image, (width, height))
-    cv2.namedWindow(image_name)
-    cv2.imshow(image_name, image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-
-def contrast_stretch(im):
-    in_min = np.percentile(im, 5)
-    in_max = np.percentile(im, 95)
-    out_min = 0.0
-    out_max = 255.0
-    out = im - in_min
-    out *= ((out_min - out_max) / (in_min - in_max))
-    out += in_min
-    return out
-
-
-def calc_ndvi(image):
-    b, g, r = cv2.split(image)
-    bottom = (r.astype(float) + b.astype(float))
-    bottom[bottom == 0] = 0.01
-    ndvi = (b.astype(float) - r) / bottom
-    return ndvi
-
-
-def contrast(image):
-    display(image, 'Original')
-    contrasted = contrast_stretch(image)
-    display(contrasted, 'Contrasted original')
-    cv2.imwrite('contrasted.png', contrasted)
-    return contrasted
-
-
-def contrastNdvi(contrasted):
-    ndvi = calc_ndvi(contrasted)
-    display(ndvi, 'NDVI')
-    ndvi_contrasted = contrast_stretch(ndvi)
-    cv2.imwrite('ndvi_contrasted.png', ndvi_contrasted)
-    display(ndvi_contrasted, 'NDVI contrasted')
-    return ndvi_contrasted
-
-
-def colorMapping(ndvi_contrasted):
-    color_mapped_prep = ndvi_contrasted.astype(np.uint8)
-    color_mapped_image = cv2.applyColorMap(color_mapped_prep, fastiecm)
-    display(color_mapped_image, 'Color mapped')
-    cv2.imwrite('color_mapped_image.png', color_mapped_image)
-    return color_mapped_image
 
 
 '''
