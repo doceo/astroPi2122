5a266115707f32dbc8c68f3933938f0f9dd77461
renatogallo27 <gallo.renato.s27@liceoviconapoli.it>
2022-02-05 18:22:12 +0100

    Sistemazione del ndvi.py e inserimento di questa funzione nel functions.py.
    Oggi io e @Biasi-Luca abbiamo sistemato l'ndvi.py rendendo tutte le funzioni dei metodi ordinate e comprensibili ad un eventuale lettore esterno. Poi abbiamo inserito il modulo dell'ndvi.py all'interno del file functions.
    
    Co-Authored-By: Biasi-Luca <74982114+Biasi-Luca@users.noreply.github.com>

--- Test/ndvi.py
+++ Test/ndvi.py
@@ -1,14 +1,16 @@
-#il seguente link spiega il codice
-#https://projects.raspberrypi.org/en/projects/astropi-ndvi
+# il seguente link spiega il codice
+# https://projects.raspberrypi.org/en/projects/astropi-ndvi
 import cv2
 import numpy as np
-#The fastie colour map takes dark pixels and makes them white. Then the brighter the original pixels, the further along the spectrum the colours are shifted. So dark grey pixels become blue, while bright white pixels become red.
+# The fastie colour map takes dark pixels and makes them white. Then the brighter the original pixels, the further
+# along the spectrum the colours are shifted. So dark grey pixels become blue, while bright white pixels become red.
 from fastiecm import fastiecm
-#from picamera import PiCamera
-#import picamera.array
 
-#these lines to your code, to setup and use the Raspberry Pi camera. Comment out the line that loads the park.png image.
-# original = cv2.imread('park.png') #Comment out this line, as no longer used
+# from picamera import PiCamera
+# import picamera.array
+
+# these lines to your code, to setup and use the Raspberry Pi camera. Comment out the line that loads the park.png
+# image. original = cv2.imread('park.png') #Comment out this line, as no longer used
 
 
 original = cv2.imread('../images/S2A_MSIL1C_20220120T091311_N0301_R050_T34TGK_20220120T111422-ql.jpg')
@@ -30,8 +32,9 @@ width = int(shape[1]/2)
 
 """
 
+
 def display(image, image_name):
-    image = np.array(image, dtype=float)/float(255)
+    image = np.array(image, dtype=float) / float(255)
     shape = image.shape
     height = int(shape[0] / 2)
     width = int(shape[1] / 2)
@@ -41,57 +44,70 @@ def display(image, image_name):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+
 def contrast_stretch(im):
-    #find the top brightness of pixels in the image in the top 5% and bottom 5% of your image.
+    # find the top brightness of pixels in the image in the top 5% and bottom 5% of your image.
     in_min = np.percentile(im, 5)
     in_max = np.percentile(im, 95)
-    #set the maximum brightness and minimum brightness on the new image you are going to create. The brightest a pixel’s colour can be is 255, and the lowest is 0.
+    # set the maximum brightness and minimum brightness on the new image you are going to create. The brightest a
+    # pixel’s colour can be is 255, and the lowest is 0.
     out_min = 0.0
     out_max = 255.0
-    """
-some calculations need to be performed to change all the pixels in the image, so that the image has the full range of contrasts from 0 to 255.
-Add these lines to stretch out the pixel values and return the contrasted image.
-"""
+    """some calculations need to be performed to change all the pixels in the image, so that the image has the full 
+    range of contrasts from 0 to 255. Add these lines to stretch out the pixel values and return the contrasted 
+    image. """
     out = im - in_min
     out *= ((out_min - out_max) / (in_min - in_max))
     out += in_min
-
     return out
 
+
 def calc_ndvi(image):
-    #To adjust the pixels in the image and only work with red and blue, the image needs splitting into its three seperate channels. r for red, g for green, and b for blue.
+    # To adjust the pixels in the image and only work with red and blue, the image needs splitting into its three
+    # seperate channels. r for red, g for green, and b for blue.
     b, g, r = cv2.split(image)
-    """
-Now the red and blue channels need to be added together and stored as bottom. The blue channel can then have the red channel subtracted (remember that red would mean unhealthy
-plants or no plants), and then divided by the bottom calculation. Because we’re doing a division, we also need to make sure that none of our divisors are 0, or there will be an error.
-"""
+    """Now the red and blue channels need to be added together and stored as bottom. The blue channel can then have 
+    the red channel subtracted (remember that red would mean unhealthy plants or no plants), and then divided by the 
+    bottom calculation. Because we’re doing a division, we also need to make sure that none of our divisors are 0, 
+    or there will be an error. """
     bottom = (r.astype(float) + b.astype(float))
-    bottom[bottom==0] = 0.01
+    bottom[bottom == 0] = 0.01
     ndvi = (b.astype(float) - r) / bottom
     return ndvi
 
 
-#display(original, 'Original')
-# convert your image and display it on the screen.
-contrasted = contrast_stretch(original)
-display(contrasted, 'Contrasted original')
-#save your high contrast image by adding a single line to the end of your code, so that you can compare the two images in your file browser. It will be called contrasted.png.
-cv2.imwrite('contrasted.png', contrasted)
-#pass in the contrasted image, display it, and save it.
-ndvi = calc_ndvi(contrasted)
-display(ndvi, 'NDVI')
-ndvi_contrasted = contrast_stretch(ndvi)
+def contrast(image):
+    display(image, 'Original')
+    # convert your image and display it on the screen.
+    contrasted = contrast_stretch(image)
+    display(contrasted, 'Contrasted original')
+    # save your high contrast image by adding a single line to the end of your code, so that you can compare the two
+    # images in your file browser. It will be called contrasted.png.
+    cv2.imwrite('contrasted.png', contrasted)
+    # pass in the contrasted image, display it, and save it.
+    return contrasted
+
+
+def contrastNdvi(contrasted):
+    ndvi = calc_ndvi(contrasted)
+    display(ndvi, 'NDVI')
+    ndvi_contrasted = contrast_stretch(ndvi)
+    cv2.imwrite('ndvi_contrasted.png', ndvi_contrasted)
+    display(ndvi_contrasted, 'NDVI contrasted')
+    return ndvi_contrasted
+
+
 """
 catch patches of brighter pixels. To once again enhance the image, it can be run through the contrast_stretch function.
 Now you can see healthy plant life by the brightness of the pixels in the ndvi_contrasted.png image.
 """
-display(ndvi_contrasted, 'NDVI Contrasted')
-cv2.imwrite('ndvi_contrasted.png', ndvi_contrasted)
-#Now the image can be converted using cv2 colour mapping, and written out as a new file
-color_mapped_prep = ndvi_contrasted.astype(np.uint8)
-#convert the image using the fastie colour map, display it, and write a new file.
-color_mapped_image = cv2.applyColorMap(color_mapped_prep, fastiecm)
-display(color_mapped_image, 'Color mapped')
-cv2.imwrite('color_mapped_image.png', color_mapped_image)
-# write out the original array to a file.
-cv2.imwrite('original.png', original)
+
+
+def colorMapping(ndvi_contrasted):
+    # Now the image can be converted using cv2 colour mapping, and written out as a new file
+    color_mapped_prep = ndvi_contrasted.astype(np.uint8)
+    # convert the image using the fastie colour map, display it, and write a new file.
+    color_mapped_image = cv2.applyColorMap(color_mapped_prep, fastiecm)
+    display(color_mapped_image, 'Color mapped')
+    cv2.imwrite('color_mapped_image.png', color_mapped_image)
+    return color_mapped_image
--- functions.py
+++ functions.py
@@ -1,4 +1,3 @@
-
 # moduli utili al daynight
 from random import randint
 from orbit import ISS
@@ -10,13 +9,16 @@ from pathlib import Path
 from time import sleep
 from datetime import datetime, timedelta
 
+# moduli utili al ndvi
+import cv2
+import numpy as np
+from fastiecm import fastiecm
 
 
 def capture(imName):
-    
-    name_image = imName +".jpg"
+    name_image = imName + ".jpg"
     camera = PiCamera()
-    camera.resolution = (1296,972)
+    camera.resolution = (1296, 972)
     # Camera warm-up time
     sleep(2)
     camera.capture(f"{name_image}")
@@ -29,12 +31,14 @@ def dayNight():
     else:
         return False
 
+
 # Define the function that create the CSV and write the firts row
 def create_csv(data_file):
     with open(data_file, 'w') as f:
         writer = csv.writer(f)
         header = ("Date/time", "Latitude", "Longitude", "Elevation")
         writer.writerow(header)
+
 
 # Define the function to write the other row with the values
 def add_csv_data(data_file, data):
@@ -43,6 +47,61 @@ def add_csv_data(data_file, data):
         writer.writerow(data)
 
 
+def display(image, image_name):
+    image = np.array(image, dtype=float) / float(255)
+    shape = image.shape
+    height = int(shape[0] / 2)
+    width = int(shape[1] / 2)
+    image = cv2.resize(image, (width, height))
+    cv2.namedWindow(image_name)
+    cv2.imshow(image_name, image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def contrast_stretch(im):
+    in_min = np.percentile(im, 5)
+    in_max = np.percentile(im, 95)
+    out_min = 0.0
+    out_max = 255.0
+    out = im - in_min
+    out *= ((out_min - out_max) / (in_min - in_max))
+    out += in_min
+    return out
+
+
+def calc_ndvi(image):
+    b, g, r = cv2.split(image)
+    bottom = (r.astype(float) + b.astype(float))
+    bottom[bottom == 0] = 0.01
+    ndvi = (b.astype(float) - r) / bottom
+    return ndvi
+
+
+def contrast(image):
+    display(image, 'Original')
+    contrasted = contrast_stretch(image)
+    display(contrasted, 'Contrasted original')
+    cv2.imwrite('contrasted.png', contrasted)
+    return contrasted
+
+
+def contrastNdvi(contrasted):
+    ndvi = calc_ndvi(contrasted)
+    display(ndvi, 'NDVI')
+    ndvi_contrasted = contrast_stretch(ndvi)
+    cv2.imwrite('ndvi_contrasted.png', ndvi_contrasted)
+    display(ndvi_contrasted, 'NDVI contrasted')
+    return ndvi_contrasted
+
+
+def colorMapping(ndvi_contrasted):
+    color_mapped_prep = ndvi_contrasted.astype(np.uint8)
+    color_mapped_image = cv2.applyColorMap(color_mapped_prep, fastiecm)
+    display(color_mapped_image, 'Color mapped')
+    cv2.imwrite('color_mapped_image.png', color_mapped_image)
+    return color_mapped_image
+
 
 '''
 QUELLO CHE SEGUE È LA PARTE CHE ANDREBBE INCLUSA IN MAIN.PY
@@ -50,24 +109,23 @@ QUELLO CHE SEGUE È LA PARTE CHE ANDREBBE INCLUSA IN MAIN.PY
 '''
 
 if __name__ == '__main__':
-
     #### ATTENZIONE, NEL MAIN VA CREATO UNA SOLA VOLTA ALTRIMENTI LO GENERA AD
     #### OGNI CICLO E LO SOVRASCRIVE SEMPRE
-    #create_csv(data_file)
+    # create_csv(data_file)
 
     ephemeris = load('de421.bsp')
     timescale = load.timescale()
 
     # Compute the coordinates of the Earth location directly beneath the ISS
-    location = ISS.coordinates() 
+    location = ISS.coordinates()
 
     base_folder = Path(__file__).parent.resolve()
-    data_file = base_folder/'data.csv'
+    data_file = base_folder / 'data.csv'
 
     image_name = datetime.now().strftime("%Y%m%d-%H%M%S")
     path_image = str(base_folder) + "/" + image_name
 
-  #  capture(path_image)
+    #  capture(path_image)
 
     row = (image_name, location.latitude, location.longitude, location.elevation.km)
     add_csv_data(data_file, row)
