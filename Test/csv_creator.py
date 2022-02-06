6fbfaa523e4bd163f099ea91418b2265b1c1a0d7
Raffaele135 <mariano.raffaele@liceoviconapoli.it>
2022-02-06 13:37:28 +0100

    Update csv_creator.py

--- Test/csv_creator.py
+++ Test/csv_creator.py
@@ -32,10 +32,9 @@ data_file = base_folder/'data.csv'
 
 name_file = datetime.now()
 row = (name_file, location.latitude.signed_dms, location.longitude.signed_dms, location.elevation.km)
-if() :
-    create_csv(data_file)
-    add_csv_data(data_file, row)
-    print('True')
+create_csv(data_file)
+add_csv_data(data_file, row)
+print(True)
 
 
 
