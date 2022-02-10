e5a3e81e7b27fa414831cae54ed23eb316f8dd52
Diomede Mazzone <diomede.mazzone@gmail.com>
2022-02-10 23:41:18 +0100

    test sul main

--- main.py
+++ main.py
@@ -29,20 +29,26 @@ if __name__ == '__main__':
         # Variables for the dayNight function
      
         light = dayNight()
-       
+        print(light)
+
         if light == True:
             
             # Compute the coordinates of the Earth location directly beneath the ISS
             location = ISS.coordinates()
-            
+            print(location)
+
             image_name = str(datetime.now().strftime("%Y%m%d-%H%M%S"))
             path_image = str(base_folder) + "/" + image_name             
             
+            print(image_name)
+
             if capture(image_name, 1):
             
                 row = (image_name, location.latitude.degrees, location.longitude.degrees, location.elevation.km)
-            
+                print(row)
                 add_csv_data(data_file, row)
         
         # Update the current time
         now_time = datetime.now()
+        print(now_time)
+        sleep(10)
