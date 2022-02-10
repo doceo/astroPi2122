e1de40ac0e18be7ae6bb2c2b249c4fb7007eb80f
Raffaele135 <mariano.raffaele@liceoviconapoli.it>
2022-02-10 16:39:24 +0100

    Update capture.py

--- Test/capture.py
+++ Test/capture.py
@@ -1,20 +1,19 @@
-from time import sleep
 from picamera import PiCamera
-from pathlib import Path
-from time import time
+from time import sleep
 from datetime import datetime, timedelta
+from pathlib import Path
+
+def capture(imName, test):
+    if test:
+        name_image = imName + ".jpg"
+        camera = PiCamera()
+        camera.resolution = (1296, 972)
+        # Camera warm-up time
+        sleep(2)
+        camera.capture(f"{name_image}")
 
 
-def capture():
-    
-    base_folder = Path(__file__).parent.resolve()
-    image_name = datetime.now().strftime("%Y%m%d-%H%M%S")
-    path_image = str(base_folder) + "/" + image_name
-    camera = PiCamera()
-    camera.resolution = (1296,972)
-    # Camera warm-up time
-    sleep(2)
-    camera.capture(f"{image_name}")
-
-capture()
-
+base_folder = Path(__file__).parent.resolve()
+image_name = datetime.now().strftime("%Y%m%d-%H%M%S")
+path_image = str(base_folder) + "/" + image_name
+capture(path_image)
