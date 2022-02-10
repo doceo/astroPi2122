a88c0d8c1037ca1d21db1fe13e1b4e43ad2eee47
Raffaele135 <mariano.raffaele@liceoviconapoli.it>
2022-02-10 17:30:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ descrizioni variabili globali
 '''
 
 start_time = datetime.now()
+now_time = datetime.now()
 
 # Initialise the CSV file
 base_folder = Path(__file__).parent.resolve()
@@ -30,7 +31,7 @@ if __name__ == '__main__':
     print('main.py - AstroPI 2021/2022')
 
     # Run a loop for three hours
-    while (now_time < start_time + timedelta(minutes=180)):
+    while (now_time < start_time + timedelta(minutes=1)):
         
         # Variables for the dayNight function
         ephemeris = load('de421.bsp')
