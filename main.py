ba82527bce6f014489d29eac0b64a5dfe4fa0d8a
Bocchetty <bocchettifrancesco06@outlook.it>
2022-02-13 17:47:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
             location = ISS.coordinates()
             print(location)
 
-            if capture(image_name, 1, dFile):
+            if capture(image_name, dFile, 1):
                 dFile = data_file
                 print("save")
                 row = (image_name, location.latitude.degrees, location.longitude.degrees, location.elevation.km)
