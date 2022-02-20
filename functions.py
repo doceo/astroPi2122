5eb0049492c853317c2a738c3ee85e02b2f62c25
Bocchetty <bocchettifrancesco06@outlook.it>
2022-02-20 18:08:45 +0100

    Update capture function

--- functions.py
+++ functions.py
@@ -14,8 +14,9 @@ def capture(imName, dFile, test):
     
     if test:
         
-        # Converting the photos in .jpg format
-        name_image = imName.split('/')[5] + ".jpg"
+        # Associate the photo with its data
+        name_image = imName.split('/')[5]
+        save_file = imName + ".jpg"
         
         # Variables for Picamera
         camera = PiCamera()
@@ -26,14 +27,14 @@ def capture(imName, dFile, test):
         print(location)
 
         # Collect and add the coordinates, related to the captured photo, to the csv
-        row = (name_image[:-4], location.latitude.degrees, location.longitude.degrees, location.elevation.km)
+        row = (name_image, location.latitude.degrees, location.longitude.degrees, location.elevation.km)
         
         # Adding the image correlated data to the CSV file
         add_csv_data(dFile, row)
         print(row)
         
         #Capturing the photo
-        camera.capture(f"{name_image}")
+        camera.capture(f"{save_file}")
         
         # Closing camera
         camera.close()
