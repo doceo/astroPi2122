86602f4c591f884d88c54b4630cf43bcc6d7b9be
Bocchetty <bocchettifrancesco06@outlook.it>
2022-04-05 17:07:15 +0200

    Update ndvi.py

--- ndvi.py
+++ ndvi.py
@@ -125,7 +125,7 @@ if __name__ == '__main__':
     
     base_folder = Path(__file__).parent.resolve()
 
-    image_name = '/images/*'
+    image_name = '/images/51845820403_7006e2b61e_o'
     image_original = str(base_folder) + image_name +'.jpg'
 
     img_master = loadImage(image_original)
