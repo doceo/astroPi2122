450df7c4aaf8c837eca35c374a7b48cda566d7f3
Bocchetty <bocchettifrancesco06@outlook.it>
2022-05-19 13:15:03 +0200

    Update redis_f.py

--- redis_f.py
+++ redis_f.py
@@ -1,39 +1,29 @@
 import redis
 
-r = redis.StrictRedis(host= '93.145.175.242', port= 63213, password='1357642rVi0', db= 0, decode_responses=True)
+r = redis.StrictRedis(host= '10.255.237.221', port= 6379, password='1357642rVi0', db= 0, decode_responses=True)
+
 # inizializzo la lista delle chiavi del DB
 r.keys()
 
-graziella=[]
-    
-giorgio=str(graziella)    
+a = "imagine"
 
-for i in range (3):
-     image_name= input()
-     row= input()
-     r.set(image_name, row)
-     value = r.get(image_name)
-     graziella.append(value)
-     n_foto = (r.incr('count'))
-     
-print(graziella)
-print (n_foto)
+for i in range (1000):
+    domanda = input('Vuoi aggiungere una foto? ' )
+    if domanda == 'si':
+        image_name= input()
+        row= input()
+        r.set(image_name, row)
+        value = r.get(image_name)
+        r.zadd(a, {row: image_name})
+        n_foto = (r.incr('count'))
+        Test= True
+    else:
+        break
 
-res = r.lrange("image_name", 0, -1)
-
-res = r.sort("image_name")
-print(res)
-
-'''''       
-def cerca():
-    str = input("inserisci la foto da cercare: ")
-    risultato = r.sismember(giorgio,str)
-    print(giorgio)
-
-    if risultato == 1:
-        print("trovato!")
-    else: print("non trovato!")
-
-cerca()
-'''
+if Test == True:
+    print ('Il numero di foto nel database è: ', n_foto)
+    ordine = r.zrange(a, 0, -1, withscores=True)
+    print('Le immagini ordinate sono:', ordine)
+else:
+    pass
 r.flushdb()
